4277487849a6836891ae6b81c0c181ffd239b0ad
henry <henry72452@gmail.com>
2026-03-20 16:48:31 +0800

    efficient way
    Signed-off-by: henry <henry72452@gmail.com>

--- IVT.py
+++ IVT.py
@@ -9,56 +9,31 @@ m = Basemap(projection='cyl', llcrnrlon=100, urcrnrlon=140,
             llcrnrlat=10, urcrnrlat=40, resolution='l')
 
 
-# June data 6/17
-rootgrp = nc.Dataset('easia_prs_jun.nc')
+def load_monthly_data(filename):
+    with nc.Dataset(filename) as ds:
+        return {
+            'zp': ds.variables['zp'][:],   # (lev, lat, lon)   
+            'qv': ds.variables['qv'][:],   # (27,  100, 140)
+            'u': ds.variables['u'][:],     # (11 = 500hPa,  15 = 700hPa, 20 = 850hPa)
+            'v': ds.variables['v'][:],     # (11 = 500h Pa,  15 = 700hPa, 20 = 850hPa)
+            'lon': ds.variables['lon'][:],
+            'lat': ds.variables['lat'][:]
+        }
 
-zp_jun = rootgrp.variables['zp'][:]   #(lev, lat, lon)
-qv_jun = rootgrp.variables['qv'][:]   #(27,  100, 140)
-u_jun = rootgrp.variables['u'][:]     #(11 = 500hPa,  15 = 700hPa, 20 = 850hPa)
-v_jun = rootgrp.variables['v'][:]
-file_lon = rootgrp.variables['lon'][:]
-file_lat = rootgrp.variables['lat'][:]
+months = ['jun', 'jul', 'aug', 'sep']
+data_list = [load_monthly_data(f'easia_prs_{month}.nc') for month in months]
 
-
-rootgrp.close()
-
-# July data 7/17
-rootgrp = nc.Dataset('easia_prs_jul.nc')
-
-zp_jul = rootgrp.variables['zp'][:]   #(lev, lat, lon)
-qv_jul = rootgrp.variables['qv'][:]   #(27,  100, 140)
-u_jul = rootgrp.variables['u'][:]     #(11 = 500hPa,  15 = 700hPa)
-v_jul = rootgrp.variables['v'][:]
-
-rootgrp.close()
-
-# August data 8/17
-rootgrp = nc.Dataset('easia_prs_aug.nc')
-
-zp_aug = rootgrp.variables['zp'][:]   #(lev, lat, lon)
-qv_aug = rootgrp.variables['qv'][:]   #(27,  100, 140)
-u_aug = rootgrp.variables['u'][:]     #(11 = 500hPa,  15 = 700hPa,  20 = 850hPa)
-v_aug = rootgrp.variables['v'][:]
-
-rootgrp.close()
-
-# September data 9/17
-rootgrp = nc.Dataset('easia_prs_sep.nc')
-
-zp_sep = rootgrp.variables['zp'][:]   #(lev, lat, lon)
-qv_sep = rootgrp.variables['qv'][:]   #(27,  100, 140)
-u_sep = rootgrp.variables['u'][:]     #(11 = 500hPa,  15 = 700hPa)
-v_sep = rootgrp.variables['v'][:]
-
-rootgrp.close()
-
-zp_tot = np.stack([zp_jun, zp_jul, zp_aug, zp_sep], axis=0)
-qv_tot = np.stack([qv_jun, qv_jul, qv_aug, qv_sep], axis=0)
-u_tot  = np.stack([u_jun, u_jul, u_aug, u_sep], axis=0)  #(4,27,100,140)
-v_tot  = np.stack([v_jun, v_jul, v_aug, v_sep], axis=0)
+zp_tot = np.stack([d['zp'] for d in data_list])
+qv_tot = np.stack([d['qv'] for d in data_list])
+u_tot = np.stack([d['u'] for d in data_list])
+v_tot = np.stack([d['v'] for d in data_list])
+lon, lat  = data_list[0]['lon'], data_list[0]['lat']
 
 
 g = 9.8
+
+dz = np.abs(zp_tot[:, 15:26, :, :] - zp_tot[:, 14:25, :, :])  # (4, 11, 100, 140)
+
 IVT = np.zeros((4,100,140))
 IVTx = np.zeros((4,100,140))
 IVTy = np.zeros((4,100,140))
